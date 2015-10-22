ba2ded893544f55051a387bf940440ffde952ed7
halvves <halvves@users.noreply.github.com>
2015-10-22 16:02:39 -0500

    rearrange structure to keep partials, js, css, etc... in their own folders

---
+++ app/js/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+var landingPageWiz = angular.module('landingPageWiz', [
+  // 'landingPageWiz.controllers',
+  // 'landingPageWiz.services',
+  'ngRoute'
+]);
+
+landingPageWiz.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/main', {
+      templateUrl: 'partials/main.html',
+    }).
+    when('/results', {
+      templateUrl: 'partials/results.html',
+    }).
+    when('/detail', {
+      templateUrl: 'partials/detail.html',
+    }).
+  otherwise({redirectTo: '/main'});
+}]);
