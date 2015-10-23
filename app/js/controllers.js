6b50aeff2a4987d5e4b2fe1decaf49a1952ef29a
halvves <halvves@users.noreply.github.com>
2015-10-23 02:07:30 -0500

    fix logic for selecting template data for detail page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,25 +1,21 @@
-landingPageWiz.controller('mainCtrl', ['$route', '$routeParams', '$location',
-  function($route, $routeParams, $location) {
-    this.$route = $route;
-    this.$location = $location;
-    this.$routeParams = $routeParams;
-}]);
+// landingPageWiz.controller('mainCtrl', ['$route', '$routeParams', '$location',
+//   function($route, $routeParams, $location) {
+//     this.$route = $route;
+//     this.$location = $location;
+//     this.$routeParams = $routeParams;
+// }]);
 
 landingPageWiz.controller('detailCtrl', ['$routeParams', 'templates', '$filter', '$scope', function($routeParams, templates, $filter, $scope) {
   $scope.controllerName = "detailCtrl";
-  this.params = $routeParams;
-	// this.tid = this.params.templateId;
-	$scope.template = templates[2];
+  $scope.params = $routeParams;
+	var tid = $routeParams.templateId;
+
 	// We filter the array by id, the result is an array
 	// so we select the element 0
-	$scope.temp = $filter('filter')(templates, function (d){return d.id === 2;})[0];
-
-	// If you want to see the result, just check the log
-	// console.log(single_object);
+	$scope.template = $filter('filter')(templates, function (d){return d.id === parseInt(tid);})[0];
 }]);
 
 landingPageWiz.controller('resultsCtrl', ['$routeParams', '$scope', 'templates', function($routeParams, $scope, templates) {
-	$scope.templates = templates;
 	$scope.controllerName = "resultsCtrl";
-  this.params = $routeParams;
+	$scope.templates = templates;
 }]);
