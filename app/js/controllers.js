aebbd8212996c3c54923b6112841c5bd51cd7949
halvves <halvves@users.noreply.github.com>
2015-10-26 15:22:20 -0500

    add more comments and to help document the functionality of 'controllers'

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,11 @@
 'use strict';
 
 landingPageWiz.controller('detailCtrl', ['$routeParams', 'templates', 'campaigns', '$filter', '$scope', function($routeParams, templates, campaigns, $filter, $scope) {
-  $scope.controllerName = "detailCtrl";
-  $scope.params = $routeParams;
+  // Collect url paramters into  variables
 	var tid = $routeParams.templateId;
   var cid = $routeParams.campaignId;
 
+  // Grab the appropriate template object from "templates" data
 	// We filter the array by id, the result is an array
 	// so we select the element 0
 	var template = $filter('filter')(templates, {id: tid})[0];
@@ -14,9 +14,19 @@ landingPageWiz.controller('detailCtrl', ['$routeParams', 'templates', 'campaigns
   if (!cid){
     cid = template.defaultCampaignId;
   }
+
+  // Grab the appropriate campaign object from "campaigns" data
   var campaign = $filter('filter')(campaigns, {id: cid})[0];
+
+  // Make an array containing other campaigns using this template
+  var otherCampaigns = $filter('filter')(campaigns, {templateId: tid});
+
+  // Pass variables needed on the front to $scope
   $scope.template = template;
   $scope.campaign = campaign;
+  $scope.otherCampaigns = otherCampaigns;
+
+  // Functionality for "Custom" style classes
   if(template.custom) {
     $scope.customFlag = "Custom";
   } else {
@@ -25,11 +35,15 @@ landingPageWiz.controller('detailCtrl', ['$routeParams', 'templates', 'campaigns
 }]);
 
 landingPageWiz.controller('mainCtrl', ['$routeParams', '$scope', 'templates', 'campaigns', '$filter', function($routeParams, $scope, templates, campaigns, $filter) {
-	$scope.controllerName = "mainCtrl";
+  // Loop through campaigns to add template title
+  // from "templates" data (based on templateId)
   for (var i=0; i < campaigns.length; i++) {
-    var tid = campaigns[i].templateId;
-    var template = $filter('filter')(templates, {id: tid})[0];
+    // Grab template
+    var template = $filter('filter')(templates, {id: campaigns[i].templateId})[0];
+    // Create a templateTitle property
     campaigns[i].templateTitle = template.title;
   }
+
+  // Pass campaigns data to $scope for use on the front
   $scope.campaigns = campaigns;
 }]);
