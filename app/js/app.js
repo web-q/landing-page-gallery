15b0f02095ddd61527214ca2a2537f0e8e673abc
halvves <halvves@users.noreply.github.com>
2016-01-04 14:09:30 -0600

    transitions almost vsmooth

--- app/js/app.js
+++ app/js/app.js
@@ -46,7 +46,7 @@ landingPageWiz.run(function($rootScope, $timeout, $window) {
     $rootScope.loadingView = false;
     $timeout(function () {
       $window.scrollTo(0,0);
-    }, 650);
+    }, 400);
   });
 });
 
